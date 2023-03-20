c96aaf7f8a5b329a4c500c826656e7cd8dd3719f
Kim Pastro <kimpastro@gmail.com>
2023-03-20 17:11:28 -0300

    working with http method badges color

---
+++ app/helpers/pulse_helper.rb
@@ -0,0 +1,14 @@
+module PulseHelper
+  HTTP_TAG_COLORS = {
+    "GET" => "text-bg-success",
+    "POST" => "text-bg-primary",
+    "PUT" => "text-bg-warning",
+    "PATCH" => "text-bg-secondary",
+    "DELETE" => "text-bg-danger",
+    "OPTIONS" => "text-bg-info"
+  }
+
+  def http_tag(http_method)
+    HTTP_TAG_COLORS[http_method]
+  end
+end
