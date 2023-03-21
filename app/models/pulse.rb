42f2d0d469cdba581422bfb5d3309e63f98c0a2d
Kim Pastro <kimpastro@gmail.com>
2023-03-21 14:53:34 -0300

    version without job to cable

--- app/models/pulse.rb
+++ app/models/pulse.rb
@@ -6,8 +6,8 @@ class Pulse < ApplicationRecord
   scope :received, -> { order(id: :desc) }
 
   after_create_commit -> { 
-    broadcast_prepend_later_to pool, target: "pulses", partial: "pulses/pulse_item", locals: { pulse: self }
-    broadcast_replace_later_to pool, target: :header_pulse_list, partial: "pulses/header_list", locals: { quantity_of_pulses: self.pool.pulses.size, total_pulses: self.pool.quantity }
+    broadcast_prepend_to pool, target: "pulses", partial: "pulses/pulse_item", locals: { pulse: self }
+    broadcast_replace_to pool, target: :header_pulse_list, partial: "pulses/header_list", locals: { quantity_of_pulses: self.pool.pulses.size, total_pulses: self.pool.quantity }
   }
 
   after_destroy_commit -> { broadcast_remove_to pool, target: "#{self.hashid}_item" }
