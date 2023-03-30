dff670935863c1b93bc0ce8dbae0791a3c92125f
Kim Pastro <kimpastro@gmail.com>
2023-03-29 21:32:08 -0300

    PulseItemComponent test

--- spec/components/pulse_item_component_spec.rb
+++ spec/components/pulse_item_component_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
-RSpec.describe PulseDetailComponent, type: :component do
+RSpec.describe PulseItemComponent, type: :component do
   let(:pool)  { create(:pool)              }
   let(:pulse) { create(:pulse, pool: pool) }
 
   context "show pulse details" do
     it "renders component" do
-      render_inline(described_class.new(pulse: pulse))
+      render_inline(described_class.new(pulse_item: pulse))
       expect(page).to have_text(pulse.hashid)
     end
   end
